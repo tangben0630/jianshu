992fbad07bfd1365552bd28b561fff295a6c9c98
tangben <240193690@qq.com>
2021-03-10 09:28:33 +0800

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Test, ContextState } from './context'
+import List from './components/List'
+import Banner from './components/Banner'
 function App() {
-  return <>
+  const value: ContextState = {
+    name: 'app,state下的数据，点击banner想修改他',
+    num: 2,
+    fn: () => {
+      console.log('fn');
+    }
+  }
+  const [con, setCon] = useState<any>(value)
 
-  </>
+  return <Test.Provider value={con}>
+    <List></List>
+    <Banner></Banner>
+  </Test.Provider>
 
 
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Cont, { dataContext } from './components/Context'
+// import Cont, { dataContext } from './components/Context'
 
 
 ReactDOM.render(
-  <Cont>
-    <App />
-  </Cont>,
+  // <Cont>
+  <App />,
+  // </Cont>,
   document.getElementById('root')
 );
 
